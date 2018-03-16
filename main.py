af9141bf41717c2a5bd256225805b5ae2bf0fee3
MohamedAhmedkassim <37451755+MohamedAhmedkassim@users.noreply.github.com>
2018-03-16 19:02:27 +0200

    Add files via upload
    until now you can just test driving from here

---
+++ main.py
@@ -0,0 +1,23 @@
+from drive import *
+from extras import *
+
+
+def main():
+    countDown(3)
+    forward()
+    right()
+    left()
+    forward()
+    right()
+    left()    
+    forward()
+    right()
+    left()
+    forward()
+    right()
+    left()
+    forward()
+    right()
+    left()    
+
+main()
