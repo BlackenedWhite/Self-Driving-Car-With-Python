ebd21c69d5ad16ba57cbbd747ff773b6782b4bd2
Amr Magdy <aaaro95@gmail.com>
2018-03-19 18:41:16 +0200

    first model

---
+++ model.py
@@ -0,0 +1,21 @@
+from keras import optimizers
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D,Activation,Dense,Flatten
+
+model = Sequential()
+
+
+model = Sequential()
+#input layer
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=(100,100,4)))
+#hidden layers
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Conv2D(64, (5, 5), activation='relu'))
+model.add(Flatten())
+#output layer
+model.add(Dense(4,activation="softmax"))
+model.compile(optimizers.Adam(),loss='categorical_crossentropy',metrics=['accuracy'])
+
+
